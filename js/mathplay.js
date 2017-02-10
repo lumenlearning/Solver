3b3cba01a5154c5c57557daa0a858b8fb74fed39
Molly LeCompte <mollyklecompte@gmail.com>
2017-02-10 15:43:03 -0800

    remove caps and underscores from changeType display.

--- js/mathplay.js
+++ js/mathplay.js
@@ -12,17 +12,20 @@ $(document).ready(function(){
       const steps = mathsteps.solveEquation(equation);
 
       steps.forEach(step => {
+        var changeTypeSpaced = step.changeType.replace(/_/g," ");
+
         $("#equationsteps").append("<strong>" + (steps.indexOf(step) + 1) + ")</strong><br>");
-        $("#equationsteps").append("before change: " + step.oldEquation.print() + "<br>");
+        $("#equationsteps").append("before change: " + changeTypeSpaced.toLowerCase() + "<br>");
         $("#equationsteps").append("change: " + step.changeType + "<br>");
         $("#equationsteps").append("after change: " + step.newEquation.print() + "<br>");
         $("#equationsteps").append("# of substeps: " + step.substeps.length + "<br><br>");
         $("#equationsteps").append("<div class='substeps'></div>");
 
         step.substeps.forEach(substep => {
+          var changeTypeSpacedSubstep = step.changeType.replace(/_/g," ");
 
           $(".substeps").append("Start with: " + substep.oldEquation.print() + "<br>");
-          $(".substeps").append("Then: " + substep.changeType + "<br>");
+          $(".substeps").append("Then: " + changeTypeSpacedSubstep.toLowerCase() + "<br>");
           $(".substeps").append("End with: " + substep.newEquation.print() + "<br><br>");
         });
       });
@@ -44,17 +47,20 @@ $(document).ready(function(){
       const simplifySteps = mathsteps.simplifyExpression(equation);
 
       simplifySteps.forEach(step => {
+        var changeTypeSpaced = step.changeType.replace(/_/g," ");
+
         $("#equationsteps").append("<strong>" + (simplifySteps.indexOf(step) + 1) + ")</strong><br>");
         $("#equationsteps").append("before change: " + step.oldNode + "<br>");
-        $("#equationsteps").append("change: " + step.changeType + "<br>");
+        $("#equationsteps").append("change: " + changeTypeSpaced.toLowerCase() + "<br>");
         $("#equationsteps").append("after change: " + step.newNode + "<br>");
         $("#equationsteps").append("# of substeps: " + step.substeps.length + "<br><br>");
         $("#equationsteps").append("<div class='substeps'></div>");
 
         step.substeps.forEach(substep => {
+          var changeTypeSpacedSubstep = step.changeType.replace(/_/g," ");
 
           $(".substeps").append("Start with: " + substep.oldNode + "<br>");
-          $(".substeps").append("Then: " + substep.changeType + "<br>");
+          $(".substeps").append("Then: " + changeTypeSpacedSubstep.toLowerCase() + "<br>");
           $(".substeps").append("End with: " + substep.newNode + "<br><br>");
         });
       });
