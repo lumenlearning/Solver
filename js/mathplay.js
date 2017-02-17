2991e392ac1f5d7cce23b1f94e2d51fe3f07ff4f
Molly LeCompte <mollyklecompte@gmail.com>
2017-02-16 22:40:10 -0800

    add complicated gross loop as step toward better step illustration, console logs.

--- js/mathplay.js
+++ js/mathplay.js
@@ -1,5 +1,15 @@
 const mathsteps = require('mathsteps');
 const $ = require('jQuery');
+const _ = require('lodash');
+
+function isInNewNode(myNode, newNode) {
+	newNode.args.forEach (arg => {
+		if (_.isEqual(arg, myNode)) {
+			return true;
+		}
+
+	});
+}
 
 
 $(document).ready(function(){
@@ -16,13 +26,51 @@ $(document).ready(function(){
 
         // console.log(steps.indexOf(step) + 1 + "OLD")
         // console.log("Left: ")
-        // console.log(step.oldEquation.leftNode);
+        // console.log(step.oldEquation.leftNode.args);
         // console.log("Right: ")
-        // console.log(step.oldEquation.rightNode);
+        // console.log(step.oldEquation.rightNode.args);
         // console.log("Right: " + step.oldEquation.rightNode);
         // console.log("NEW");
-        console.log("Left: " + step.newEquation.leftNode.changeGroup);
-        console.log("Right: " + step.newEquation.rightNode.changeGroup);
+        // console.log("Left: " + step.newEquation.leftNode.args);
+        // console.log("Right: " + step.newEquation.rightNode.args);
+
+        var leftArgsOld = step.oldEquation.leftNode.args;
+        var rightArgsOld = step.oldEquation.rightNode.args;
+        var leftArgsNew = step.newEquation.leftNode.args;
+        var rightArgsNew = step.newEquation.rightNode.args;
+
+        // if (leftArgsOld) {
+        //     leftArgsOld.forEach(arg => {
+        //         console.log("step " + (steps.indexOf(step) +1) + " old left: " + arg.type);
+        //         console.log(arg);
+        //     });
+        //     console.log(step.oldEquation.leftNode.op);
+        //     console.log(step.oldEquation.leftNode.changeGroup);
+        // }
+        // if (rightArgsOld) {
+        //     rightArgsOld.forEach(arg => {
+        //         console.log("step " + (steps.indexOf(step) +1) + " old right: " + arg.type);
+        //         console.log(arg);
+        //     });
+        //     console.log(step.oldEquation.rightNode.op);
+        //     console.log(step.oldEquation.rightNode.changeGroup);
+        // }
+        // if (leftArgsNew) {
+        //     leftArgsNew.forEach(arg => {
+        //         console.log("step " + (steps.indexOf(step) +1) + " new left: " + arg.type);
+        //         console.log(arg);
+        //     });
+        //     console.log(step.newEquation.leftNode.op);
+        //     console.log(step.newEquation.leftNode.changeGroup);
+        // }
+        // if (rightArgsNew) {
+        //     rightArgsNew.forEach(arg => {
+        //         console.log("step " + (steps.indexOf(step) +1) + " new right: " + arg.type);
+        //         console.log(arg);
+        //         });
+        //     console.log(step.newEquation.rightNode.op);
+        //     console.log(step.newEquation.rightNode.changeGroup);
+        // }
 
         $("#equationsteps").append("<strong>" + (steps.indexOf(step) + 1) + ")</strong><br>");
         $("#equationsteps").append("before change: " + step.oldEquation.print() + "<br>");
@@ -30,10 +78,52 @@ $(document).ready(function(){
         $("#equationsteps").append("after change: " + step.newEquation.print() + "<br>");
         $("#equationsteps").append("<div class='substeps'></div>");
 
+
+
+
         step.substeps.forEach(substep => {
+            if (substep.changeType === 'ADD_POLYNOMIAL_TERMS') {
+                var polynomials = [];
+                if (substep.oldEquation.leftNode.args) {
+                    var newNode = substep.newEquation.leftNode;
+                    substep.oldEquation.leftNode.args.forEach(oldArg => {
+                        if (oldArg.args) {
+                            oldArg.args.forEach(oldArgNarrow => {
+                                newNode.args.forEach(newArg => {
+                                    if (newArg.args) {
+                                        newArg.args.forEach(newArgNarrow => {
+                                            if (_.isEqual(oldArgNarrow, newArgNarrow) === false) {
+                                                polynomials.push(oldArgNarrow);
+                                            }
+                                        });
+                                    }
+
+                                });
+                            });
+                        }
+                    });
+                }
+
+                console.log("POLYNOMIALS");
+                console.log("polynomials: " + polynomials);
+            }
+            // else {
+            //     var newNode = substep.newEquation.rightNode;
+            //     substep.oldEquation.rightNode.args.forEach(arg => {
+            //         if (isInNewNode(arg, newNode) === false) {
+            //             polynomials.push(arg);
+            //         }
+            //     });
+            // }
+
+            console.log("SUBSTEP");
+            console.log("old left: " );
+            console.log(substep.oldEquation.leftNode.args);
+            // console.log("old right: " + substep.oldEquation.rightNode.changeGroup);
+            console.log("new left: ");
+            console.log(substep.newEquation.leftNode.args);
+            // console.log("new right: " + substep.newEquation.rightNode.changeGroup);
           var changeTypeSpacedSubstep = substep.changeType.replace(/_/g," ");
-          console.log("OLD EQUATION LEFT NODE: " + substep.oldEquation.leftNode.changeGroup);
-          console.log("OLD EQUATION RIGHT NODE: " + substep.oldEquation.rightNode.changeGroup);
 
           $(".substeps").append("Start with: " + substep.oldEquation.print() + "<br>");
           $(".substeps").append("Then: " + changeTypeSpacedSubstep.toLowerCase() + "<br>");
